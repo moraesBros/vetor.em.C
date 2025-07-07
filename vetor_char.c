8da3bb10e44ff82334cde641bb321d74f06c7657
moraesBros <161986858+moraesBros@users.noreply.github.com>
2025-07-07 19:30:06 -0300

    Create vetor_char.c

---
+++ vetor_char.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+//declarando e inicializando o vetor
+    char al[] = "0123456789";
+    printf("al[0] %c\n", al[0]);
+    printf("al[2] %c\n", al[2]);
+    printf("al[5] %c\n", al[5]);
+    printf("al[8] %c\n", al[8]);
+    return 0;
+}
