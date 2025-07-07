548b6d043420b1077fafea11b2c46347b25a9b6c
moraesBros <161986858+moraesBros@users.noreply.github.com>
2025-07-07 19:15:43 -0300

    Create vetor_float.c

---
+++ vetor_float.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+//declarando e inicializando o vetor
+    float v[4] = {7.1, 8.4, 9.5, 5.2};
+    printf("v[0] %f\n", v[0]);
+    printf("v[1] %f\n", v[1]);
+    printf("v[2] %f\n", v[2]);
+    printf("v[3] %f\n", v[3]);
+    return 0;
+}
